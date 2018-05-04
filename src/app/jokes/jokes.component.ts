2a12d88cb78af56b1c310941756ff9616a9028e8
Bhavesh Kumar <bhaveshkumar19856@gmail.com>
2018-05-04 23:21:29 +0530

    feature increment

--- src/app/jokes/jokes.component.ts
+++ src/app/jokes/jokes.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jokes.component.css']
 })
 export class JokesComponent implements OnInit {
-
+  title = 'Chuck Norris';
+  jokes = getJokes()
+          .then(jokes => jokes.value)
+          .catch( err => console.log(err));
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+async function getJokes() {
+  const res = await fetch ('http://api.icndb.com/jokes/random/10');
+  const data = await res.json();
+  return data;
 }
